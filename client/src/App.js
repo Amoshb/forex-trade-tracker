d22343bd25c38a6f17fcfa91330963b00ba4794b
Amoshb <amosbalami8@gmail.com>
2026-04-04 14:30:01 +0100

    inital project structure

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginBottom: "20px" }}>
+      <h1>Forex Trade Tracker</h1>
+      <p>Track your forex trades with ease!</p>
+      <h2>Register</h2>
+    </div>
+  );
+}
+
+export default App;
