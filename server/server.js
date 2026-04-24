5ec6af31b8be2c032f5199e94a7ac1e7328b2d80
Amoshb <amosbalami8@gmail.com>
2026-04-24 19:38:28 +0100

    Fix cors

--- server/server.js
+++ server/server.js
@@ -8,14 +8,17 @@ const tradeRoutes = require("./Route/trade-routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-connectToDB();
+
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
 
+connectToDB();
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
